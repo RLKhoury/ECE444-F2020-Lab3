2bd7ffd542209e568f76be1cb6a47d872ea5a284
RLKhoury <rakanlkhoury@gmail.com>
2020-09-29 11:54:44 -0400

    Completed Activity 2 - Chapter 4

--- hello.py
+++ hello.py
@@ -1,15 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime as dt
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fghjtybnruvm10192837465'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=dt.utcnow())
+    form = HelloForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash("You've changed your name!")
+        if old_email is not None and old_email != form.email.data:
+            flash("You've changed your email!")
+            
+        session['name'] = form.name.data
+        session['email'] = form.email.data if utoronto_email(form.email.data) else None
+        
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time=dt.utcnow(), form=form, name=session.get('name'), email=session.get('email'))
     
     
 @app.route('/user/<name>')
@@ -24,8 +41,21 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-    
+
+def utoronto_email(email):
+    if "utoronto" in email:
+        return True
+    return False
+
+
+class HelloForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[Email()])
+    submit = SubmitField('Submit')
+
+        
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
+   
     
